222b95dbf05e73b3cd9f900567b04737bba23f08
sayudhalw <mazaya.studio@outlook.com>
2022-07-02 12:32:33 +0700

    + Add Button API

--- src/buttons/Buttons.tsx
+++ src/buttons/Buttons.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const Button = () => {
-	return <button> Hello World </button>;
-};
+export const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<'button'>
+  >(({ children, ...props }, ref) => {
+  return (
+    <button ref={ref} type="button" {...props}>
+      {children}
+    </button>
+  );
+});
